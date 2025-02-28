fb452ded2225323d93305d0dc4ae5694990c8c04
macfarley90 <therealmccoyster@gmail.com>
2025-02-28 15:38:48 -0500

    added audio, fixed victory/loss messages

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-//make sure the game loaded properly
-console.log('game loaded')
 
 //cached html stuff here
+let mainTitleEl = document.querySelector('#mainTitle')
 let dagronMsgEl = document.querySelector('#dagronMessage')
 let warriorMsgEl = document.querySelector('#warriorMessage')
+let dagronPictureEl = document.querySelector('.messages')
 let dagronHpEl = document.querySelector('#dagronHp')
 let villagerCountEl = document.querySelector('#villagerCount')
 let warriorHpEl = document.querySelector('#warriorHp')
@@ -22,19 +22,20 @@ function dTen(){
 //other variables
 let incomingDmg = 0;
 let warriorDmg = 0;
-let dagronCurrentHP = dagron.health
-let warriorCurrentHP = warrior.health
-let warriorMsg = warriorMsgEl.innerText
-let dagronMsg = dagronMsgEl.innerText
-let breath = true;
-let flying = false;
-let playerChoice = '';
-let shield = false;
 let casualties = 0;
-let villagersLeft = warrior.villagers
-let gameOver = false;
 let dagronChoice = 0;
 let vitality = 0;
+let dagronCurrentHP; 
+let warriorCurrentHP; 
+let warriorMsg;
+let dagronMsg;
+let villagersLeft;
+let breath = true;
+let flying = false;
+let shield = false;
+let gameOver = false;
+let victory = false;
+let playerChoice = '';
 const actions= [biteAttack, smashBuilding, fly, tailAttack, dragonFire, dragonFire]
 
 //display stuff to the user
@@ -42,6 +43,13 @@ function updateMessage (){
     //what you did, what the dragon did
     dagronMsgEl.innerText = dagronMsg
     warriorMsgEl.innerText = warriorMsg
+    if(gameOver == false && victory == false){
+      mainTitleEl.innerText = 'Slay the Dagron'
+    }else if(gameOver == true){
+        mainTitleEl.innerText = 'GAME OVER'
+    }else if(victory == true){
+        mainTitleEl.innerText = "VICTORY"
+    }
 }
 
 function updateGameState(){
@@ -69,7 +77,7 @@ const getPlayerChoice = (event) => {
     //where event is the click
         playerChoice = event.target.dataset.action;
         //the name of the thing you clicked
-        console.log('playerChoice')
+        console.log(playerChoice)
     };
 //if the player chooses attack
 function spearAttack(){
@@ -98,7 +106,7 @@ function defend(){
 function healthPot(){
     //10d6+10
     vitality = (dSix()+dSix()+dSix()+dSix()+dSix()+dSix()+dSix()+dSix()+dSix()+dSix())+10;
-    console.log(vitality)
+    console.log(vitality +" HP healed")
     warriorMsg = `You feel a rush of vitality when you quaff a potion and heal for ${vitality} Hit Points`
     warriorCurrentHP = warriorCurrentHP+vitality
 }
@@ -112,6 +120,8 @@ function runAway(){
 function resetGame(){
     nextTurn()
     gameOver = false;
+    victory = false;
+    mainTitleEl.innerText = 'Slay the Dagron'
     dagronCurrentHP = dagron.health;
     warriorCurrentHP = warrior.health;
     villagersLeft = warrior.villagers
@@ -119,13 +129,14 @@ function resetGame(){
     warriorMsg = "Throw spears until the Dagron is defeated!"
     render()
     console.log('game reset')
- 
 }
 
 //stuff the dragon does
 function actionDie(){
+    if(victory == true){
+        return;
+    }
     dagronChoice = dSix()
-    console.log(`action dice roll is ${dagronChoice}`)
     actions[dagronChoice -1]()
     }
 function biteAttack(){
@@ -140,6 +151,7 @@ function biteAttack(){
         shield = false
         return}
         else{warriorCurrentHP = warriorCurrentHP - incomingDmg}
+        console.log("Dagron bites!")
     }
 function smashBuilding(){
     if(shield == true){
@@ -150,9 +162,10 @@ function smashBuilding(){
     console.log(`${casualties} smashed`)
     dagronMsg = `The dragon smashes a nearby building in a rage and ${casualties} villagers were crushed under the rubble.`
     villagersLeft = villagerCountEl.innerText - casualties
-    }}
+    console.log('Dagron smash!')
+}}
 function fly(){
-    console.log('fly')
+    console.log('dragon flies')
     flying = true
     dagronMsg = "The draon takes flight and swoops out of range."
 }
@@ -164,6 +177,7 @@ function tailAttack(){
         villagersLeft = villagersLeft- casualties
         } else {
         dagronMsg = `You manage to dodge a sweep of the Dagron's tail.  If only the ${casualties} nearby villagers were as nimble as you.`
+        console.log("tail attack")
     }
     if(shield == true){
         warriorCurrentHP = warriorHpEl.innerText - Math.floor(incomingDmg/2)
@@ -185,25 +199,28 @@ function dragonFire(){
         if(shield == true){
             warriorCurrentHP = warriorCurrentHP - Math.floor(incomingDmg/2)}
                 else{warriorCurrentHP = warriorCurrentHP - incomingDmg}
+        console.log("dagron burns")
         return
     }else if(breath == false){
         //charge up breath, next 5 or 6 on action die breathes fire
             breath = true
             dagronMsg = "The Dagron rears back and a great wind fills its capacious lungs. Beware! The next burst of flame could come at any second."
+            console.log('dagron breathes in to charge his fire')
         }
+
     }
 
 //if dragon health reaches 0 we win
 function checkForWin(){
     if(dagronCurrentHP >= 0){
         return
-    } else{
-        dagronMsg = 'With that last spear the Dagron is overwhelmed, it falls dead to the ground.'
-        gameOver = true
+    } else if(dagronCurrentHP <= 0){
+        dagronMsg = `With that last spear thrust, the Dagron is overwhelmed, it falls dead to the ground.  You saved the remaining ${villagersLeft} villagers.`
+        victory = true
         render()
-        return
+        return}
     }
-}
+
 //if villagers or HP reaches 0
 function checkForLoss(){
     if(warriorCurrentHP > 0 && villagersLeft > 0){
@@ -228,6 +245,7 @@ function nextTurn(){
     playerChoice = '';
     dagronMsg = '';
     warriorMsg = '';
+    resetButtonEl.innerText = 'Reset the Game'
 }
 
 //run through what happens when you click
@@ -267,7 +285,16 @@ function handleClick(event){
     nextTurn()
 }
 
+//on page load reset the game.
+window.addEventListener("DOMContentLoaded", event => {
+   resetGame()
+  });
 
-
+//added a fun dragon roar from open source audio  
+dagronPictureEl.addEventListener('click', event =>{
+    const audio = document.querySelector('audio')
+    audio.volume = 0.2;
+    audio.play()
+} )
 
 
